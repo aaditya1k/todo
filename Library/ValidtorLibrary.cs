e1fbb86b94975f0085de55fa8d6c2ff2a774d3a3
Aaditya Singh <aaditya.coder@gmail.com>
2017-04-21 10:34:06 +0530

    Validator class for validating email

---
+++ Library/ValidtorLibrary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Net.Mail;
+
+namespace Todo.Library
+{
+    public class ValidtorLibrary
+    {
+        public bool IsValidEmailAddress(String email)
+        {
+            try
+            {
+                MailAddress m = new MailAddress(email);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+    }
+}
